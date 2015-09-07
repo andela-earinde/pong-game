4de858752b9f80352126ce960bc3a12ed67c3d77
Udo Nkwocha <writeudo@gmail.com>
2015-09-07 16:54:43 +0100

    added connection to socket server

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,10 @@
 'use strict';
-
 $(document).ready(function() {
+  var socket = io('http://localhost:7000');
+
+  socket.on('connect', function() {
+    console.log('connected to socket server');
+  });
 
   var modal = $('#signupModal');
   var saveBtn = $('#save-btn');

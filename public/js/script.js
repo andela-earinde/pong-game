71cf86f7ab20df7c1d0040d98b0d741344c8bdc2
Udo Nkwocha <writeudo@gmail.com>
2015-09-07 17:26:27 +0100

    added client connection

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,40 @@
 'use strict';
 $(document).ready(function() {
-  var socket = io('http://localhost:7000');
-
-  socket.on('connect', function() {
-    console.log('connected to socket server');
-  });
-
+  var socket;
   var modal = $('#signupModal');
   var saveBtn = $('#save-btn');
-  modal.modal({
-    backdrop: 'static',
-    keyboard: false
-  });
-  modal.modal('show');
+
+  var username = localStorage.getItem('pong-username');
+  if (username === null) {
+    modal.modal({
+      backdrop: 'static',
+      keyboard: false
+    });
+    modal.modal('show');
+  }
 
   saveBtn.on('click', function(e) {
     e.preventDefault();
-    modal.modal('hide');
+    var name = $.trim($('#username').val());
+    if (name.length > 2) { // use regex later
+      localStorage.setItem('pong-username', name);
+      username = name;
+      modal.modal('hide');
+    }
   });
+
+  if (username !== null) {
+    try {
+      socket = io.connect('http://localhost:7000');
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  socket.on('connect', function(data) {
+    console.log('connected to socket server');
+    socket.emit('user_joined', username);
+  });
+
 
 });

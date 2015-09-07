1be54895c54b360ea4df22237ad456bd14e9a03a
Arinde Eniola <eniola.arinde@andela.co>
2015-09-07 22:11:05 +0100

    updates

--- server.js
+++ server.js
@@ -19,19 +19,33 @@ var server = app.listen(config.port, function() {
 var io = require('socket.io').listen(server);
 
 var users = {};
+var request = {};
 
 io.sockets.on('connection', function(socket) {
 
-  socket.on("user_joined", function(username) {
-    socket.emit("user_id", socket.id);
+  socket.on('user_joined', function(username) {
+    socket.emit('user_id', socket.id);
     socket.username = username;
     users[username] = [username, socket.id];
     io.sockets.emit("person_joined", users);
   });
 
-  socket.on("disconnect", function() {
+  socket.on('request_player', function(data) {
+    if(request[data.name]) {
+      request[data.name].push({id:socket.id,
+                              name: socket.username});
+    }
+    else {
+      request[data.name] = [];
+      request[data.name].push({id:socket.id,
+                              name: socket.username});
+    }
+    socket.broadcast.to(data.id).emit('player_request', request[data.name]);
+  });
+
+  socket.on('disconnect', function() {
     delete users[socket.username];
-    socket.broadcast.emit("person_left", users);
+    socket.broadcast.emit('person_left', users);
   });
 
 });

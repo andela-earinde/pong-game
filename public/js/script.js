1644dd40783769cbb29d95c34233ab8f13b77f9e
Udo Nkwocha <writeudo@gmail.com>
2015-09-07 21:35:01 +0100

    updated offline checkers

--- public/js/script.js
+++ public/js/script.js
@@ -4,13 +4,18 @@ $(document).ready(function() {
   var saveBtn = $('#save-btn');
   var personsOnline = $('.persons-online');
   var server = 'http://192.168.101.222:7000';
+  var status = $('#connection-status');
+  var personsName = $('#persons-name');
 
   var updatePlayersOnline = function(users) {
     var html = '';
+    var userId = localStorage.getItem('pong-id');
     for (var i in users) {
       if (users.hasOwnProperty(i)) {
         var person = users[i];
-        html += '<a href="' + person[1] + '" class="list-group-item">' + person[0] + '<i class="fa fa-spinner pull-right"></i> <span class="label label-success pull-right">online</span></a>';
+        if (userId !== person[1]) {
+          html += '<a href="' + person[1] + '" class="list-group-item">' + person[0] + '<i class="fa fa-spinner pull-right"></i> <span class="label label-success pull-right">online</span></a>';
+        }
       }
     }
     personsOnline.html(html);
@@ -31,6 +36,7 @@ $(document).ready(function() {
     modal.modal('show');
   } else {
     socket.emit('user_joined', username);
+    personsName.html(username);
   }
 
   saveBtn.on('click', function(e) {
@@ -41,13 +47,20 @@ $(document).ready(function() {
       username = name;
       modal.modal('hide');
       socket.emit('user_joined', username);
+      personsName.html(username);
     }
   });
 
   if (socket !== undefined) {
 
     socket.on('connect', function(data) {
+      status.html('Online').removeClass('label-default').addClass('label-success');
       console.log('connected to socket server');
+    });
+
+    socket.on('disconnect', function() {
+      status.html('Offline').removeClass('label-success').addClass('label-default');
+      console.log('disconnected from server');
     });
 
     socket.on('user_id', function(user_id) {
